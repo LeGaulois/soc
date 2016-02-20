9d22372a550362409aa71fc5437403ddbe755b5a
Krazylec <evangelus95@gmail.com>
2016-02-20 22:40:00 +0100

    modif urls

--- sources/serveurs/urls.py
+++ sources/serveurs/urls.py
@@ -3,7 +3,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
-    	url(r'accueil/$', views.liste,name='liste'),
+	url(r'^$', views.liste,name='liste'),
+    	url(r'^liste/$', views.liste,name='liste'),
 	url(r'^identite/(?P<ip>([0-9]{1,3}\.){3}([0-9]{1,3}))/$', views.identite,name='identite'),
 	url(r'^edit/(?P<ip>([0-9]{1,3}\.){3}([0-9]{1,3}))/$', views.edit,name='edit'),
 	url(r'^suppression/(?P<ip>([0-9]{1,3}\.){3}([0-9]{1,3}))/$', views.suppression,name='suppression'),
