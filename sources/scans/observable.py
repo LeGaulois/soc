ed3a09832b23e8d27679c5ead41773291ac7b4d3
LeGaulois <florian@legaulois.ovh>
2016-05-12 14:23:44 +0200

    erreurs fichier debug

--- sources/scans/observable.py
+++ sources/scans/observable.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 from django.conf import settings
 
 BASE=settings.BASE_DIR+'/'
-#BASE='/var/www/html/django/sources/'
+
 
 class Observable(object):
     def __init__(self):
@@ -15,20 +15,12 @@ class Observable(object):
         On crée un thread pour le traitement de chaque notification
         '''
 
-        
-        f=open(BASE+"obs.log",'a')
-        f.write('*'*60)
-        f.write('\n')
-        f.write(str(kwargs)+'\n')
-        f.close()
-        
-
         for obs in self.observers[event]:
             thread =Thread(target=obs.notify,args=(self,),kwargs=kwargs)
             thread.start()
-            
+
     def add_observer(self, obs,event):
         if not hasattr(obs, 'notify'):
             raise ValueError("L'observer doit posseder la methode 'notify'")
-    
+
         self.observers[event].append(obs)
